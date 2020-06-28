502fa865ae84a96b07b6820de25e09e21ed438de
Konstantin Mamaev <meison@meison.net>
2020-06-28 17:27:26 +0300

    refactor: переносит вызов библиотеки в index.js, переписывает for → map для сравнения значений объектов

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,16 @@
 #!/usr/bin/env node
+import pkg from 'commander';
+import genDiff from '../index.js';
 
-import { cli } from '../index.js';
+const { Command } = pkg;
 
-cli.parse(process.argv);
+const program = new Command();
+program
+  .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format', 'stylish')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2, program.format));
+  })
+  .parse(process.argv);
--- index.js
+++ index.js
@@ -1,22 +1,93 @@
-// @ts-check
-import pkg from 'commander';
-import genDiff from './src/gendiff.js';
+import path from 'path';
+import fs from 'fs';
+import lodash from 'lodash';
+import types from './src/types.js';
+import parsers from './src/parsers/index.js';
 
-const { Command } = pkg;
+import formatters from './src/formatters/index.js';
 
-const program = new Command();
-program
-  .version('0.0.1')
-  .description('Compares two configuration files and shows a difference.');
-program
-  .option('-f, --format [type]', 'output format', 'stylish');
+const { has, union, isObject } = lodash;
 
-program
-  .arguments('<filepath1> <filepath2>')
-  .action((filepath1, filepath2) => {
-    console.log(genDiff(filepath1, filepath2, program.format));
+const createMeta = (key, value, type) => ({
+  value,
+  key,
+  type,
+});
+
+const getNotModifiedMeta = (key, value) => createMeta(key, value, types.notModified);
+
+const getAddedMeta = (key, value) => createMeta(key, value, types.added);
+
+const getDeletedMeta = (key, value) => createMeta(key, value, types.deleted);
+
+const getChangedMeta = (key, value, newValue) => ({
+  ...createMeta(key, value, types.changed),
+  newValue,
+});
+
+const comparer = (obj1, obj2) => {
+  const allKeys = union(Object.keys(obj1), Object.keys(obj2));
+
+  const result = allKeys.map((key) => {
+    if (!has(obj1, key)) {
+      return getAddedMeta(key, obj2[key]);
+    }
+
+    if (!has(obj2, key)) {
+      return getDeletedMeta(key, obj1[key]);
+    }
+
+    if (isObject(obj1[key]) && isObject(obj2[key])) {
+      return {
+        ...getNotModifiedMeta(key),
+        children: comparer(obj1[key], obj2[key]),
+      };
+    }
+
+    if (obj1[key] !== obj2[key]) {
+      return getChangedMeta(key, obj1[key], obj2[key]);
+    }
+
+    return getNotModifiedMeta(key, obj1[key]);
   });
 
-export const cli = program;
+  return result;
+};
 
-export default genDiff;
+const readFile = (pathToFile) => fs.readFileSync(path.resolve(pathToFile), 'utf-8');
+
+const gendiff = (pathToFile1, pathToFile2, format = 'stylish') => {
+  const extension = path.extname(pathToFile1);
+  const fileContent1 = readFile(pathToFile1);
+  const fileContent2 = readFile(pathToFile2);
+
+  let parsedContent1;
+  let parsedContent2;
+
+  switch (extension) {
+    case '.yml':
+      parsedContent1 = parsers.yml(fileContent1);
+      parsedContent2 = parsers.yml(fileContent2);
+      break;
+    case '.ini':
+      parsedContent1 = parsers.ini(fileContent1);
+      parsedContent2 = parsers.ini(fileContent2);
+      break;
+    default:
+      parsedContent1 = parsers.json(fileContent1);
+      parsedContent2 = parsers.json(fileContent2);
+  }
+
+  const diffMeta = comparer(parsedContent1, parsedContent2);
+
+  switch (format) {
+    case 'json':
+      return diffMeta;
+    case 'plain':
+      return formatters.plain(diffMeta);
+    default:
+      return formatters.stylish(diffMeta);
+  }
+};
+
+export default gendiff;
