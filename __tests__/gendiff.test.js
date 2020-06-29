7ec6a823d46759e0e9be691183ceb2ce1a291573
Konstantin Mamaev <meison@meison.net>
2020-06-29 16:39:43 +0300

    test: переименовывает тест

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -6,7 +6,7 @@ import genDiff from '../index.js';
 const getFixturePath = (filename) => path.join(process.cwd(), '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-describe('output formatting', () => {
+describe('gendiff', () => {
   test('stylish', () => {
     const result = genDiff(getFixturePath('complexFile1.json'), getFixturePath('complexFile2.json'), 'stylish');
     const out = readFile('complexFileOutStylish.txt');
