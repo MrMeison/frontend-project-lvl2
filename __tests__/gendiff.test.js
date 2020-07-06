b49ae0efef7b92b6ee983fabc7ac58023926b0c1
Konstantin Mamaev <meison@meison.net>
2020-07-06 20:11:25 +0300

    test: добавляет тесты на разные форматы

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -7,28 +7,20 @@ const getFixturePath = (filename) => path.join(process.cwd(), '__fixtures__', fi
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 describe('gendiff', () => {
-  let filePath1;
-  let filePath2;
-
-  beforeAll(() => {
-    filePath1 = getFixturePath('complexFile1.json');
-    filePath2 = getFixturePath('complexFile2.json');
-  });
-
   test('stylish', () => {
-    const result = genDiff(filePath1, filePath2, 'stylish');
+    const result = genDiff(getFixturePath('complexFile1.json'), getFixturePath('complexFile2.json'), 'stylish');
     const out = readFile('complexFileOutStylish.txt');
     expect(result.trim()).toBe(out);
   });
 
   test('plain', () => {
-    const result = genDiff(filePath1, filePath2, 'plain');
+    const result = genDiff(getFixturePath('complexFile1.json'), getFixturePath('complexFile2.yml'), 'plain');
     const out = readFile('complexFileOutPlain.txt');
     expect(result.trim()).toBe(out);
   });
 
   test('json', () => {
-    const result = JSON.parse(genDiff(filePath1, filePath2, 'json'));
+    const result = JSON.parse(genDiff(getFixturePath('complexFile1.json'), getFixturePath('complexFile2.ini'), 'json'));
     const out = JSON.parse(readFile('complexFileOut.json'));
     expect(result).toEqual(out);
   });
--- src/parsers/index.js
+++ src/parsers/index.js
@@ -1,8 +1,27 @@
 import ini from 'ini';
+import lodash from 'lodash';
 import yaml from 'js-yaml';
 
+const { mapValues, isObject } = lodash;
+
+const isNumeric = (value) => typeof value === 'string' && !Number.isNaN(Number(value));
+
+const convertValues = (obj) => mapValues(obj, (value) => {
+  if (isObject(value)) {
+    return convertValues(value);
+  }
+
+  if (isNumeric(value)) {
+    return Number(value);
+  }
+
+  return value;
+});
+
+const parseIni = (content) => convertValues(ini.parse(content));
+
 const mapParsers = {
-  ini: ini.parse,
+  ini: parseIni,
   yml: yaml.safeLoad,
   json: JSON.parse,
 };
